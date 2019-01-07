c1d993daf4bfe67a552aff414ab31e00f5d9762d
Daljeet Virdi <daljeetv@gmail.com>
2019-01-07 12:58:30 -0800

    Use environment variables for stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_bTXjdoNyXnGbsmNC0HSCHC9r',
-    :secret_key => 'sk_test_c99Ms4WEX14mebtkwLWacAdX'
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
---
+++ docs/HEROKU.md
@@ -0,0 +1,6 @@
+1. To delete a user: 
+    ```
+    $ heroku run rails console
+    $ User.find(1).destroy
+    ```
+2. 
