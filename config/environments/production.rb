016edf601a736d9dab0209bf596ee43915dc8fc1
Daljeet Virdi <daljeetv@gmail.com>
2019-01-26 15:42:05 -0800

    add prod image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
   config.lograge.enabled = true
 
   config.skylight.probes += %w(redis) if ENV['SKYLIGHT_AUTHENTICATION'].present?
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 end
